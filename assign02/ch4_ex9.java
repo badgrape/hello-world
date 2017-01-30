d999f5b495ccff1784ce2b60c10d615e1e8db580
badgrape <jimalexmorris@gmail.com>
2017-01-30 15:48:47 -0500

    Add files via upload

--- assign02/ch4_ex9.java
+++ assign02/ch4_ex9.java
@@ -9,13 +9,47 @@ public class ch4_ex9 {
 		System.out.print("What kind of operation would you like to perform? (+ - * /) ");
 		String op = console.next();
 		
-				
+		System.out.print("Enter the first number: ");
+		int num1 = console.nextInt();
+		
+		System.out.print("Enter the second number: ");
+		int num2 = console.nextInt();
+		
+		int result = 0;
+		int divrm = 0;
+		
+		boolean valid = true;
+		
 		switch (op) {
 		
 		case "+":
-			String[] terms = {"augend", "addend", "sum"};
+			result = num1 + num2;
+			break;
+		case "-":
+			result = num1 - num2;
+			break;
+		case "*":
+			result = num1 * num2;
+			break;
+		case "/":
+			if (num2 !=0) {
+				result = num1 / num2;
+				divrm = num1 % num2;
+			}
+			else {
+				System.out.println("You can't divide a number by zero. Run the program again.");
+			}
+			break;
+		default: valid = false;
 		}
-		
+						
+		if (valid == true) {
+			System.out.printf(num1 + op + num2 + " = " + result
+				+ (op.equals("/") && divrm != 0 ? " R" + divrm : ""));
+		}
+		else {
+			System.out.println("Invalid entry. Run the program again.");
+		}
 		
 	}
 	
